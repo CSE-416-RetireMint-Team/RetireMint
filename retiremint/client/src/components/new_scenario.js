e3c2de305c9bf6b53028dadae1b6353bdb094355
1314Czyckagy123 <123561882+1314Czyckagy123@users.noreply.github.com>
2025-03-13 17:43:29 -0400

    starting

---
+++ retiremint/client/src/components/new_scenario.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+function New_scenario({ set_current_page }) {
+    return (
+        <div>
+            <Header />
+            <p>Create a New Scenario</p>
+                
+            
+
+        </div>
+    );
+}
+
+export default New_scenario;
