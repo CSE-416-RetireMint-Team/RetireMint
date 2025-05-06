6e54d316926447e729a4b758c514169d2c21b336
schaite <sabrinachaite25@gmail.com>
2025-05-05 23:41:34 -0400

    Import without allocations

--- retiremint/server/src/ImportScenario/handleInvestments.js
+++ retiremint/server/src/ImportScenario/handleInvestments.js
@@ -3,43 +3,46 @@ const InvestmentType = require('../Schemas/InvestmentType');
 const Investment = require('../Schemas/Investments'); 
 
 function mapReturnOrIncome(dist, amtOrPct = 'percent') {
-    const stdDev = dist.stdev ?? dist.sd;
-  
-    if (dist.type === 'fixed') {
-      return {
-        method: amtOrPct === 'amount' ? 'fixedValue' : 'fixedPercentage',
-        ...(amtOrPct === 'amount'
-          ? { fixedValue: dist.value }
-          : { fixedPercentage: dist.value })
-      };
-    } else if (dist.type === 'normal') {
-      return {
-        method: amtOrPct === 'amount' ? 'normalValue' : 'normalPercentage',
-        ...(amtOrPct === 'amount'
-          ? { normalValue: { mean: dist.mean, sd: stdDev } }
-          : { normalPercentage: { mean: dist.mean, sd: stdDev } })
-      };
-    } else if (dist.type === 'uniform') {
-      return {
-        method: amtOrPct === 'amount' ? 'uniformValue' : 'uniformPercentage',
-        ...(amtOrPct === 'amount'
-          ? {
-              uniformValue: {
-                lowerBound: dist.lower,
-                upperBound: dist.upper
-              }
+  const stdDev = dist.stdev ?? dist.sd;
+
+  // Convert percentage fields to decimals
+  const convert = (val) => amtOrPct === 'percent' ? val * 100 : val;
+
+  if (dist.type === 'fixed') {
+    return {
+      method: amtOrPct === 'amount' ? 'fixedValue' : 'fixedPercentage',
+      ...(amtOrPct === 'amount'
+        ? { fixedValue: convert(dist.value) }
+        : { fixedPercentage: convert(dist.value) })
+    };
+  } else if (dist.type === 'normal') {
+    return {
+      method: amtOrPct === 'amount' ? 'normalValue' : 'normalPercentage',
+      ...(amtOrPct === 'amount'
+        ? { normalValue: { mean: convert(dist.mean), sd: convert(stdDev) } }
+        : { normalPercentage: { mean: convert(dist.mean), sd: convert(stdDev) } })
+    };
+  } else if (dist.type === 'uniform') {
+    return {
+      method: amtOrPct === 'amount' ? 'uniformValue' : 'uniformPercentage',
+      ...(amtOrPct === 'amount'
+        ? {
+            uniformValue: {
+              lowerBound: convert(dist.lower),
+              upperBound: convert(dist.upper)
             }
-          : {
-              uniformPercentage: {
-                lowerBound: dist.lower,
-                upperBound: dist.upper
-              }
-            })
-      };
-    } else {
-      throw new Error(`Unknown distribution type: ${dist.type}`);
-    }
-  }  
+          }
+        : {
+            uniformPercentage: {
+              lowerBound: convert(dist.lower),
+              upperBound: convert(dist.upper)
+            }
+          })
+    };
+  } else {
+    throw new Error(`Unknown distribution type: ${dist.type}`);
+  }
+}
 
   async function handleInvestments(yamlData, userId) {
     const nameToTypeMap = new Map();
@@ -47,6 +50,7 @@ function mapReturnOrIncome(dist, amtOrPct = 'percent') {
     const investments = yamlData.investments || [];
     const savedInvestments = [];
     let totalInitialCash = 0;
+    const afterTaxLimit = yamlData.afterTaxContributionLimit ?? 0;
   
     // Step 1: Create investment types, skipping "cash"
     for (const type of investmentTypes) {
@@ -70,7 +74,7 @@ function mapReturnOrIncome(dist, amtOrPct = 'percent') {
           description: type.description,
           expectedAnnualReturn: returnDist._id,
           expectedAnnualIncome: incomeDist._id,
-          expenseRatio: type.expenseRatio,
+          expenseRatio: type.expenseRatio != null ? type.expenseRatio * 100 : 0,
           taxability
         });
   
@@ -84,33 +88,33 @@ function mapReturnOrIncome(dist, amtOrPct = 'percent') {
     for (const inv of investments) {
       if (inv.investmentType.toLowerCase() === 'cash') {
         totalInitialCash += inv.value || 0;
-        continue; // Don't create cash investment
+        continue;
       }
-  
+
       const typeId = nameToTypeMap.get(inv.investmentType);
       if (!typeId) {
         throw new Error(`Unknown investmentType name: ${inv.investmentType}`);
       }
-  
+
       const investmentType = await InvestmentType.findById(typeId);
       if (!investmentType) throw new Error(`Invalid investmentType ID: ${typeId}`);
-  
+
       const isTaxable = investmentType.taxability === 'taxable';
       const taxStatus = inv.taxStatus;
-  
+
       if (isTaxable && !taxStatus) {
         throw new Error(`Investment "${inv.id}" is taxable and must include a taxStatus`);
       }
-  
+
       if (!isTaxable) {
         inv.taxStatus = undefined;
         inv.maxAnnualContribution = undefined;
       }
-  
-      if (taxStatus === 'after-tax' && inv.maxAnnualContribution == null) {
-        inv.maxAnnualContribution = 0;
+
+      if (taxStatus === 'after-tax') {
+        inv.maxAnnualContribution = afterTaxLimit;
       }
-  
+
       const investmentDoc = new Investment({
         name: inv.id,
         investmentType: typeId,
@@ -118,15 +122,16 @@ function mapReturnOrIncome(dist, amtOrPct = 'percent') {
         accountTaxStatus: taxStatus,
         maxAnnualContribution: inv.maxAnnualContribution
       });
-  
+
       const savedInvestment = await investmentDoc.save();
       savedInvestments.push(savedInvestment);
     }
-  
-    return {
-      investmentIds: savedInvestments.map(inv => inv._id),
-      initialCash: totalInitialCash
-    };
-  }  
+
+      
+        return {
+          investmentIds: savedInvestments.map(inv => inv._id),
+          initialCash: totalInitialCash
+        };
+      }  
 
 module.exports = handleInvestments;
